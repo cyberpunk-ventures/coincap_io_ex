a622d237d099256b3623e64bcd07a4a3013666a1
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2016-03-17 21:28:35 +0200

    json response schema to docs

--- lib/coincap_io.ex
+++ lib/coincap_io.ex
@@ -10,9 +10,31 @@ defmodule CoincapIo do
      |> Poison.Parser.parse!
   end
 
+  @doc """
+  this endpoint returns list of elements with the JSON schema as in example:
+  {
+    position24: "1",
+    position: "1",
+    short: "BTC",
+    long: "Bitcoin",
+    time: 1458233596106,
+    price: 419.19,
+    perc: "0.90",
+    volume: "83980200",
+    usdVolume: "83980200",
+    cap24hrChange: "0.90",
+    mktcap: 6425092806,
+    supply: "15327400",
+    published: false,
+    vwapData: 419.18999999999994,
+    vwapDataBTC: 0.9999999999999999,
+    shapeshift: true
+  }
+  """
   @spec front :: List.t()
   def front do
     {:ok, response} = CoincapIo.get("/front")
     response.body
   end
+
 end
